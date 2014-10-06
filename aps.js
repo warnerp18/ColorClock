4972baaf82f9b7baf48c4cbe0dbf589046db2706
warnerp18 <warnerp18@gmail.com>
2014-10-06 14:42:29 -0500

    test

--- aps.js
+++ aps.js
@@ -3,4 +3,22 @@ window.onload = app;
 function app() {
     "use strict";
 
+    function count() {
+        var d = new Date();
+        var hh = d.getHours(),
+            mm = d.getMinutes(),
+            ss = d.getSeconds();
+
+        var time = hh + ":" + mm + ":" + ss;
+        document.write(time);
+
+        span.textContent = time.join(":")
     }
+
+
+
+    var spann = document.querySelector('span');
+    var h1 = document.querySelector('h1')
+
+    setInterval(count, 1000);
+}
